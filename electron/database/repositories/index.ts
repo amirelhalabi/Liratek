ba6d1480fe00dd27089124f957e70ab631e641ae
amirelhalabi <amir619halabi@gmail.com>
2025-12-20 15:42:01 +0200

    chore(electron): sync base repository and export indexes from dev

--- electron/database/repositories/index.ts
+++ electron/database/repositories/index.ts
@@ -3,60 +3,30 @@
  */
 
 // Base Repository
-export { BaseRepository } from "./BaseRepository";
-export type {
-  BaseEntity,
-  FindOptions,
-  UpdateOptions,
-  PaginatedResult,
-} from "./BaseRepository";
+export { BaseRepository } from './BaseRepository';
+export type { BaseEntity, FindOptions, UpdateOptions, PaginatedResult } from './BaseRepository';
 
 // User Repository
-export {
-  UserRepository,
-  getUserRepository,
-  resetUserRepository,
-} from "./UserRepository";
-export type {
-  UserEntity,
-  SafeUser,
-  CreateUserData,
-  UpdateUserData,
-} from "./UserRepository";
+export { UserRepository, getUserRepository, resetUserRepository } from './UserRepository';
+export type { UserEntity, SafeUser, CreateUserData, UpdateUserData } from './UserRepository';
 
 // Product Repository
-export {
-  ProductRepository,
-  getProductRepository,
-  resetProductRepository,
-} from "./ProductRepository";
-export type {
-  ProductEntity,
-  ProductDTO,
-  CreateProductData,
+export { ProductRepository, getProductRepository, resetProductRepository } from './ProductRepository';
+export type { 
+  ProductEntity, 
+  ProductDTO, 
+  CreateProductData, 
   UpdateProductData,
   StockStats,
-  LowStockProduct,
-} from "./ProductRepository";
+  LowStockProduct 
+} from './ProductRepository';
 
 // Client Repository
-export {
-  ClientRepository,
-  getClientRepository,
-  resetClientRepository,
-} from "./ClientRepository";
-export type {
-  ClientEntity,
-  CreateClientData,
-  UpdateClientData,
-} from "./ClientRepository";
+export { ClientRepository, getClientRepository, resetClientRepository } from './ClientRepository';
+export type { ClientEntity, CreateClientData, UpdateClientData } from './ClientRepository';
 
 // Sales Repository
-export {
-  SalesRepository,
-  getSalesRepository,
-  resetSalesRepository,
-} from "./SalesRepository";
+export { SalesRepository, getSalesRepository, resetSalesRepository } from './SalesRepository';
 export type {
   SaleEntity,
   SaleItemEntity,
@@ -70,114 +40,67 @@ export type {
   TopProduct,
   RecentSale,
   ChartDataPoint,
-} from "./SalesRepository";
+} from './SalesRepository';
 
 // Debt Repository
-export {
-  DebtRepository,
-  getDebtRepository,
-  resetDebtRepository,
-} from "./DebtRepository";
-export type {
-  DebtLedgerEntity,
-  DebtorSummary,
-  TopDebtor,
-  DebtSummary,
-  CreateRepaymentData,
-} from "./DebtRepository";
+export { DebtRepository, getDebtRepository, resetDebtRepository } from './DebtRepository';
+export type { 
+  DebtLedgerEntity, 
+  DebtorSummary, 
+  TopDebtor, 
+  DebtSummary, 
+  CreateRepaymentData 
+} from './DebtRepository';
 
 // Exchange Repository
-export {
-  ExchangeRepository,
-  getExchangeRepository,
-  resetExchangeRepository,
-} from "./ExchangeRepository";
-export type {
-  ExchangeTransactionEntity,
-  CreateExchangeData,
-} from "./ExchangeRepository";
+export { ExchangeRepository, getExchangeRepository, resetExchangeRepository } from './ExchangeRepository';
+export type { ExchangeTransactionEntity, CreateExchangeData } from './ExchangeRepository';
 
 // Financial Service Repository (OMT, WHISH, BOB)
-export {
-  FinancialServiceRepository,
-  getFinancialServiceRepository,
-  resetFinancialServiceRepository,
-} from "./FinancialServiceRepository";
-export type {
-  FinancialServiceEntity,
-  CreateFinancialServiceData,
-  ProviderStats,
-  FinancialServiceAnalytics,
-} from "./FinancialServiceRepository";
+export { FinancialServiceRepository, getFinancialServiceRepository, resetFinancialServiceRepository } from './FinancialServiceRepository';
+export type { 
+  FinancialServiceEntity, 
+  CreateFinancialServiceData, 
+  ProviderStats, 
+  FinancialServiceAnalytics 
+} from './FinancialServiceRepository';
 
 // Rate Repository
-export {
-  RateRepository,
-  getRateRepository,
-  resetRateRepository,
-} from "./RateRepository";
-export type { ExchangeRateEntity, SetRateData } from "./RateRepository";
+export { RateRepository, getRateRepository, resetRateRepository } from './RateRepository';
+export type { ExchangeRateEntity, SetRateData } from './RateRepository';
 
 // Currency Repository
-export {
-  CurrencyRepository,
-  getCurrencyRepository,
-  resetCurrencyRepository,
-} from "./CurrencyRepository";
-export type {
-  CurrencyEntity,
-  CreateCurrencyData,
-  UpdateCurrencyData,
-} from "./CurrencyRepository";
+export { CurrencyRepository, getCurrencyRepository, resetCurrencyRepository } from './CurrencyRepository';
+export type { CurrencyEntity, CreateCurrencyData, UpdateCurrencyData } from './CurrencyRepository';
 
 // Recharge Repository
-export {
-  RechargeRepository,
-  getRechargeRepository,
-  resetRechargeRepository,
-} from "./RechargeRepository";
-export type { VirtualStock, RechargeData } from "./RechargeRepository";
+export { RechargeRepository, getRechargeRepository, resetRechargeRepository } from './RechargeRepository';
+export type { VirtualStock, RechargeData } from './RechargeRepository';
 
 // Maintenance Repository
-export { MaintenanceRepository } from "./MaintenanceRepository";
-export type { MaintenanceJob } from "./MaintenanceRepository";
+export { MaintenanceRepository } from './MaintenanceRepository';
+export type { MaintenanceJob } from './MaintenanceRepository';
 
 // Settings Repository
-export {
-  SettingsRepository,
-  getSettingsRepository,
-  resetSettingsRepository,
-} from "./SettingsRepository";
-export type { SettingEntity } from "./SettingsRepository";
+export { SettingsRepository, getSettingsRepository, resetSettingsRepository } from './SettingsRepository';
+export type { SettingEntity } from './SettingsRepository';
 
 // Expense Repository
-export {
-  ExpenseRepository,
-  getExpenseRepository,
-  resetExpenseRepository,
-} from "./ExpenseRepository";
-export type { ExpenseEntity, CreateExpenseData } from "./ExpenseRepository";
+export { ExpenseRepository, getExpenseRepository, resetExpenseRepository } from './ExpenseRepository';
+export type { ExpenseEntity, CreateExpenseData } from './ExpenseRepository';
 
 // Closing Repository
-export {
-  ClosingRepository,
-  getClosingRepository,
-  resetClosingRepository,
-} from "./ClosingRepository";
-export type {
-  DailyClosingEntity,
-  ClosingAmountEntity,
-  DrawerBalances as ClosingDrawerBalances,
-  SystemExpectedBalances,
+export { ClosingRepository, getClosingRepository, resetClosingRepository } from './ClosingRepository';
+export type { 
+  DailyClosingEntity, 
+  ClosingAmountEntity, 
+  DrawerBalances as ClosingDrawerBalances, 
+  SystemExpectedBalances, 
   DailyStatsSnapshot,
   OpeningBalanceAmount,
-  ClosingAmount,
-} from "./ClosingRepository";
+  ClosingAmount
+} from './ClosingRepository';
 
 // Activity Repository
-export {
-  ActivityRepository,
-  getActivityRepository,
-  resetActivityRepository,
-} from "./ActivityRepository";
-export type { ActivityLogEntity, SyncErrorEntity } from "./ActivityRepository";
+export { ActivityRepository, getActivityRepository, resetActivityRepository } from './ActivityRepository';
+export type { ActivityLogEntity, SyncErrorEntity } from './ActivityRepository';
